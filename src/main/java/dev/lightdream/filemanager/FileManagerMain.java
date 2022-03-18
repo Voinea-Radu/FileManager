0150fc2ee2093fbe5412d386d6ee419c623d9a76
L1ghtDream <radu.voinea.mihai@gmail.com>
2022-03-18 03:58:13 +0200

    Added version print-out

--- src/main/java/dev/lightdream/filemanager/FileManagerMain.java
+++ src/main/java/dev/lightdream/filemanager/FileManagerMain.java
@@ -1,8 +1,18 @@
 package dev.lightdream.filemanager;
 
+import dev.lightdream.lambda.LambdaExecutor;
+import dev.lightdream.logger.LoggableMain;
+
 import java.io.File;
 
 public interface FileManagerMain {
+
+    @SuppressWarnings("unused")
+    static String getVersion() {
+        return "FileManager " + FileManagerMain.class.getPackage().getImplementationVersion() +
+                "    -> " + LoggableMain.getVersion() +
+                "    -> " + LambdaExecutor.getVersion();
+    }
 
     File getDataFolder();
 
