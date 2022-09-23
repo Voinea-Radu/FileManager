2442483f63088b1440e1f54c8cc59f20bbeeedbe
Radu Voinea <radu.voinea.mihai@gmail.com>
2022-09-23 22:14:46 +0300

    Refactor

--- src/main/java/dev/lightdream/filemanager/FileManagerMain.java
+++ src/main/java/dev/lightdream/filemanager/FileManagerMain.java
@@ -1,8 +1,5 @@
 package dev.lightdream.filemanager;
 
-import dev.lightdream.lambda.LambdaExecutor;
-import dev.lightdream.logger.LoggableMain;
-
 import java.io.File;
 
 public interface FileManagerMain {
